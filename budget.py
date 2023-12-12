809fdd626be012ae0842ef8f2fb04aeaf46a24d8
Rimelabed <151995136+Rimelabed@users.noreply.github.com>
2023-12-12 21:29:33 +0100

    Update budget.py

--- budget.py
+++ budget.py
@@ -1,4 +1,23 @@
 class Category:
+  ledger_list=[]
+  def __init__(self,name):
+        self.name = name
+        self.ledger = []
+    
+  def deposit(self,amount,description=""):
+    self.ledger.append({"amount":amount,"description":description})
+    
+  def withdraw(self,amount,price):
+    if (self.checkfunds(amount)):
+      self.ledger.append({"amount":-amount,"description":description})
+      return true
+    return false
+  def get_balance(self):
+    total=0
+    for item in self.ledger:
+      total=total+ item["amount"]
+    return total
+    
 
 
 
